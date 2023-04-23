c679b14a456c75be17ac52798164cd6fedfd7d4f
joaosantos99 <jfss291@gmail.com>
2023-04-23 20:08:07 +0100

    chores: removed boilerplate from App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 import React, { Component } from 'react';
-import { calculateResult, detectSums } from './utils';
 import Calculator from './components/Calculator';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      userInput: '',
-      result: '',
-      error: 'Write something',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    const { value } = event.target;
-    this.setState({ value });
-  }
-
-  handleSubmit(event) {
-    const { value } = this.state;
-    const { input, result, error } = calculateResult(value);
-    this.setState({ userInput: input, result, error });
-    event.preventDefault();
-
-    detectSums([1, 2, 1, 2, 3]);
-  }
-
   render() {
-    const { userInput, result, error } = this.state;
     return (
       <div className="App">
         <Calculator />
